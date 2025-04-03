2c83b22378ed92c869459b3ce005168c81eb93e5
Maryjanesilva <ribeirodasilvam083@gmail.com>
2025-04-03 14:06:54 -0300

    livewire-admin e funcionario

---
+++ app/Livewire/Admin/Adimin/AdminCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Adimin;
+
+use Livewire\Component;
+
+class AdminCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.adimin.admin-create');
+    }
+}
---
+++ app/Livewire/Admin/Adimin/AdminEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Adimin;
+
+use Livewire\Component;
+
+class AdminEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.adimin.admin-edit');
+    }
+}
